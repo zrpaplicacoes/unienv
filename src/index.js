89385d911abe9eae7e4465abd89c5c7c573162b4
Leonardo Costa <leonardoteck@gmail.com>
2019-08-07 18:37:01 -0300

    list not ignored files

--- src/index.js
+++ src/index.js
@@ -5,19 +5,21 @@
  * x verifica se não tem nenhum "unienv stash push"
  * x git stash push --include-untracked -m "unienv stash push"
  * x listar entradas do .env
- * _ verifica se tem "unienv-prefix" no .env (se tiver, com o valor @, por exemplo, vai buscar por @API_URL ao invés de API_URL)
- * _ listar arquivos ignorados (.gitignore) // não sei como
+ * x listar arquivos ignorados (.gitignore) // não sei como
  * _ procurar em todos os arquivos pelas entradas do .env, com exceção dos ignorados (usar regex e "match whole word")
+ * _ verifica se tem "unienv-prefix" no .env (se tiver, com o valor @, por exemplo, vai buscar por @API_URL ao invés de API_URL)
  * _ substituir os resultados da busca pelos valores
  */
 
-const APP_ROOT = require('app-root-path');
+const APP_ROOT = require('app-root-path').toString();
 const { STASH_PUSH_NAME_PREFIX } = require('./constants');
+const path = require('path');
 const {
   getHeadInfo,
   getStashList,
   executeCommand,
   readEnv,
+  listFiles,
 } = require('./methods');
 
 (async (args) => {
@@ -36,7 +38,10 @@ const {
       await executeCommand(`git stash push --include-untracked -m "${stashName}"`);
       await executeCommand('git stash apply --index');
 
+      const fileList = await listFiles(APP_ROOT, path.join(APP_ROOT, '.gitignore'));
+      fileList.forEach((file) => {
 
+      });
     } // else if (args[0] === '--help') {}
 
 
--- src/methods.js
+++ src/methods.js
@@ -1,6 +1,46 @@
 const { exec } = require('child_process');
 const fs = require('fs');
+const path = require('path');
+const ignore = require('ignore');
 const { Stash } = require('./classes');
+
+async function replaceInFile(filePath, searchPattern, value) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, null, (error, data) => {
+      if (error) reject(error);
+      const result = data.toString().replace(searchPattern, value);
+      fs.writeFile(filePath, result, null, (error) => {
+        if (error) throw error;
+      });
+    });
+  });
+}
+
+function listFiles(folderPath, ignoreFilePath) {
+  const ign = ignore().add(fs.readFileSync(ignoreFilePath).toString()).add(['.git', '.gitignore']);
+  function recurse(recurseFolder) {
+    const entryPaths = fs.readdirSync(recurseFolder);
+    const filePaths = entryPaths.filter(entryPath => fs.statSync(path.join(recurseFolder, entryPath)).isFile() && !ign.ignores(entryPath));
+    const dirPaths = entryPaths.filter(entryPath => !filePaths.includes(entryPath)
+      && (entryPath.endsWith('/') ? !ign.ignores(entryPath) : !ign.ignores(`${entryPath}/`)));
+    const dirFiles = dirPaths.reduce((prev, curr) => prev.concat(recurse(curr)), []);
+    return [...filePaths.map(entry => path.join(recurseFolder, entry)), ...dirFiles.map(entry => path.join(recurseFolder, entry))];
+  }
+  return recurse(folderPath);
+}
+
+/**
+ * @param {string} filePath
+ * @returns {string}
+ */
+async function readTextFile(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, (error, data) => {
+      if (error) reject(error);
+      resolve(data.toString());
+    });
+  });
+}
 
 /**
  * Reads and parse the indicated file for a list of key-value pairs
@@ -9,19 +49,14 @@ const { Stash } = require('./classes');
  * @return {Array<{ key: string, value: string }>}
  */
 async function readEnv(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, (error, data) => {
-      if (error) reject(error);
-
-      const keyValueList = {};
-      data.toString().split('\n').forEach((line) => {
-        if (line.trim() === '') return;
-        const [key, value] = line.split('=');
-        keyValueList.push({ key, value });
-      });
-      resolve(keyValueList);
-    });
+  const data = await readTextFile(filePath);
+  const keyValueList = {};
+  data.toString().split('\n').forEach((line) => {
+    if (line.trim() === '') return;
+    const [key, value] = line.split('=');
+    keyValueList.push({ key, value });
   });
+  return keyValueList;
 }
 
 function executeCommand(command) {
@@ -70,4 +105,6 @@ module.exports = {
   getStashList,
   getHeadInfo,
   executeCommand,
+  listFiles,
+  replaceInFile,
 };

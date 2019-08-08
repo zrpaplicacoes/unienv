f1aff64ea3dc7d5d71b300837b821d9d323c3558
Leonardo Costa <leonardoteck@gmail.com>
2019-08-08 16:49:31 -0300

    revert working properly

--- src/index.js
+++ src/index.js
@@ -18,14 +18,15 @@ const {
       const envFile = envFileArg ? envFileArg.substring(4).replace(/["']/g, '') : `${APP_ROOT}/.env`;
       const envEntries = await readEnv(envFile);
 
-      const prefixArg = args.find(arg => /prefix=/gi.test(arg));
-      const prefix = prefixArg ? prefixArg.substring(7) : '';
+      const prefixArg = args.find(arg => /--prefix=/gi.test(arg));
+      const prefix = prefixArg ? prefixArg.substring(9) : '';
 
+      const currentBranch = await executeCommand('git rev-parse --abbrev-ref HEAD');
       console.log('Creating unienv branch to save your work...');
       await executeCommand('git checkout -b unienv', true);
       await executeCommand('git add -A', true);
       console.log('Commiting your work...');
-      await executeCommand('git commit --allow-empty -m "original changes"', true);
+      await executeCommand(`git commit --allow-empty -m "unienv: original changes from branch ${currentBranch}"`, true);
 
       console.log(`Applying environment values from file ${path.basename(envFile)}...`);
       const fileList = await listFiles(APP_ROOT, path.join(APP_ROOT, '.gitignore'));
@@ -36,14 +37,25 @@ const {
         });
         return writeTextFile(filePath, fileContent);
       });
+
       console.log('Commiting environment values application to revert at a later point...');
       await executeCommand('git add -A', true);
-      await executeCommand('git commit -m "env applied"', true);
-      console.log('Done.');
-    } else if (args[0] === 'revert') {
-      await executeCommand('git revert --no-edit --no-commit HEAD', true);
-      await executeCommand('git reset --mixed HEAD~2', true);
+      await executeCommand('git commit -m "unienv: env values applied"', true);
 
+      console.log('Done.');
+      console.log('\n    !====================================!');
+      console.log('     CAUTION: DO NOT DO ANY GIT OPERATION\n     before runing "unienv revert" or the\n      revert might not work as expected!');
+      console.log('    !====================================!\n');
+    } else if (args[0] === 'revert') {
+      const previousBranch = (await executeCommand('git log --format=%B -n 1 HEAD~1')).trim().substring(37);
+      console.log('Removing environment values from source code');
+      await executeCommand('git revert --no-edit --no-commit HEAD', true);
+      console.log('Retrieving your saved work');
+      await executeCommand('git reset --mixed HEAD~2', true);
+      await executeCommand(`git checkout ${previousBranch}`, true);
+      console.log('Cleaning everything');
+      await executeCommand('git branch -D unienv', true);
+      console.log('Done. You can now commit and push your changes ;)');
     }
 
     process.exit(0);
